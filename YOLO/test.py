b8bf87c16cd0da56f0b5caeb33ad1e8f102e9e6c
TimmmofeyD <timmmofey@itachi-uchiha.ru>
2024-08-24 01:27:12 +0300

    1st yolo train

--- YOLO/test.py
+++ YOLO/test.py
@@ -2,4 +2,6 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8s-obb.pt')
 
-results = model.train(data='Yolo_dataset/data.yml', epohcs=5, imgsz=640, batch=2)
+results = model.train(data='Yolo_dataset/data.yaml', epochs=1, imgsz=640, batch=2)
+
+model.save('runs\\obb\\train11\\weights\\best.pt')
