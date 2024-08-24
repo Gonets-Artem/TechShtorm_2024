774500cd49316bf765bbd774e6dd78a5da0f5766
Artem Gonets <argontomos@gmail.com>
2024-08-25 01:42:18 +0300

    update structure

--- proj/test.py
+++ proj/test.py
@@ -4,7 +4,7 @@ import cv2
 standart_path = 'C:\\Users\\ArgonV\\Documents\\Учеба\\Универ\\Хакатон 2\\TechShtorm_2024'
 
 # Загрузка сохранённой модели
-model = YOLO(f'{standart_path}\\runs\\detect\\train15\\weights\\best.pt')
+model = YOLO(f'{standart_path}\\runs\\detect\\train16\\weights\\best.pt')
 
 
 # train11 - видео
@@ -21,27 +21,39 @@ model = YOLO(f'{standart_path}\\runs\\detect\\train15\\weights\\best.pt')
 # mess
 # try
 
-video_path = f"{standart_path}\\proj\\videos\\021_mess_both_s.mp4"
+video_path = f"{standart_path}\\proj\\videos\\002_keep_green_s.mp4"
 cap = cv2.VideoCapture(video_path)
 
 # Настройка для сохранения результата в виде видео
-output_path = 'output_video.mp4'
+output_path = 'proj\\output_video_now.mp4'
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter(output_path, fourcc, 30.0, (int(cap.get(3)), int(cap.get(4))))
 
 while cap.isOpened():
     ret, frame = cap.read()
-    print(frame.shape)
     
     if not ret:
         break
     
     # Применение модели на каждом кадре
-    results = model(frame)
+    results = model(frame, conf=0.5)
     
     # Получение аннотированного изображения
+
+    #boxes = results[0].boxes if hasattr(results[0], 'boxes') else None
+    # Проверка и вывод boxes
+    #if boxes is not None:
+        #print(len(boxes))
+        #print("Boxes:", boxes.xywhn)
+        #print(boxes.cls)
+        #print(boxes.conf)
+        #for el in boxes:
+            
+        #print(boxes[0]['cls'])
+
+    #boxes.
     annotated_frame = results[0].plot()
-    
+
     # Запись аннотированного кадра в выходное видео
     out.write(annotated_frame)
     
