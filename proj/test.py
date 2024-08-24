ae0c3356ffd78e5d97b9ba62fd7d363278ef0a31
Artem Gonets <argontomos@gmail.com>
2024-08-24 16:19:32 +0300

    add all data

--- proj/test.py
+++ proj/test.py
@@ -1,19 +1,33 @@
 from ultralytics import YOLO
 import cv2
 
+standart_path = 'C:\\Users\\ArgonV\\Documents\\Учеба\\Универ\\Хакатон 2\\TechShtorm_2024'
+
 # Загрузка сохранённой модели
-model = YOLO('C:\\Users\\Huawei\\Desktop\\proj\\runs\\detect\\train19\\weights\\best.pt')
+model = YOLO(f'{standart_path}\\runs\\detect\\train15\\weights\\best.pt')
 
-# Открытие видео
-video_path = "C:\\Users\\Huawei\\Desktop\\proj\\005_keep_red_s.mp4"
+
+# train11 - видео
+#C:\Users\ArgonV\Documents\Учеба\Универ\Хакатон 2\TechShtorm_2024\runs\detect\train11\weights\best.pt
+
+# train19 - фотограааафия
+#{standart_path}\\runs\\detect\\train11\\weights\\best.pt
+
+# train15 - видео 021_mess
+#{standart_path}\\runs\\detect\\train15\\weights\\best.pt
+
+# goal
+# keep
+# mess
+# try
+
+video_path = f"{standart_path}\\proj\\videos\\021_mess_both_s.mp4"
 cap = cv2.VideoCapture(video_path)
-
-
 
 # Настройка для сохранения результата в виде видео
 output_path = 'output_video.mp4'
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter(output_path, fourcc, 20.0, (int(cap.get(3)), int(cap.get(4))))
+out = cv2.VideoWriter(output_path, fourcc, 30.0, (int(cap.get(3)), int(cap.get(4))))
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -28,7 +42,6 @@ while cap.isOpened():
     # Получение аннотированного изображения
     annotated_frame = results[0].plot()
     
-
     # Запись аннотированного кадра в выходное видео
     out.write(annotated_frame)
     
--- proj/train.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO('yolov8s.pt')
-
-results = model.train(data="C:\\Users\\Huawei\\Desktop\\proj\\data.yaml", epochs=2, imgsz=640, batch=8)
-
